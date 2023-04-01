90a039161bf95357c4c34740bc9b7e32f6cea6b3
RileyHall99 <120997568+RileyHall99@users.noreply.github.com>
2023-04-01 10:52:13 -0700

    Added in my code
    Nothing is optimized, but will run all the way through

--- src/style_transfer/chromosome.py
+++ src/style_transfer/chromosome.py
@@ -1,14 +1,27 @@
-import random
 import numpy as np
+import random 
+
+import matplotlib.image
+from PIL import Image as img
+from scipy import signal as sg
+import scipy.misc
+from perlin_noise import PerlinNoise
+import random 
+import cv2
 
 
+SIZE = 80
 class Chromosome:
     # track next chrom ID
     id_count = 0
-
-    def __init__(self, genes, gene_count):
+   
+    def __init__(self , genes):
         self.genes = genes
-        self.gene_count = gene_count
+        self.gene_count = len(self.genes)
+        #Variables used to calculate fitness
+        self.size = 80
+        self.content_image = r"C:\\Users\\RileyHall\\Documents\\Applied AI\\final\\resizedImages\\0@9L6C8Y7J9U.jpg"
+        self.style_image = r"C:\\Users\\RileyHall\\Documents\\Applied AI\\final\\resizedImages\\0A3O3S3H9D1Q.jpg"
         # assigns unique ID to each chrom
         self.chrom_index = Chromosome.next_id()
 
@@ -26,10 +39,206 @@ class Chromosome:
 
     # calculate the fitness score (number of 1s in the genes)
     def get_fitness(self):
-        return sum(self.genes)
+        style_loss = self.style_loss()
+        
+        content_loss = self.content_loss()
+        
+        return content_loss + style_loss
 
     # format the chromosome genes and fitness score as a string
     def to_string(self):
         return "Chromosome: " + str(self.chrom_index) \
-               + " Genes: " + ''.join(str(i) for i in self.genes) \
-               + " Fitness: " + str(self.get_fitness())
+            + " Genes: " + ''.join(str(i) for i in self.genes) \
+            + " Fitness: " + str(self.get_fitness())
+        
+
+
+#Generate intial generations
+    @classmethod
+    def generate(self): 
+        seed = random.randint(1,100)
+        
+        noise = PerlinNoise(octaves=10, seed=seed)
+        # image = img.open(self.content_image)
+        # image_rgb = list(image.getdata())   
+        
+        # self.individual.genes = [[noise([i/len(image_rgb),j/len(image_rgb)]for j in range(len(image_rgb)))]for i in range(len(image_rgb))]
+        return [[noise([i/SIZE, j/SIZE]) for j in range(SIZE)] for i in range(SIZE)] 
+
+
+#RILEY HALL CODE,,, ALL LOSS FUNCTIONS 
+
+    def content_loss(self): 
+
+        new_genes_arr = []
+       
+        for i in range(len(self.genes)): 
+
+            if(type(self.genes[i])!=float):
+                new_genes_arr.append(self.genes[i])
+
+        #grey scale the images 
+        content_image = img.open(self.content_image)
+        content_image_rgb = list(content_image.getdata())
+
+
+        grey_content = np.dot(np.array(content_image_rgb)[...,:3],[0.2989, 0.5870, 0.1140])
+        # grey_content = np.dot(image_rgb,[0.299, 0.587, 0.114])[...,None]
+        generated = np.dot(np.array(new_genes_arr)[...,:3], [0.2989, 0.5870, 0.1140])
+        #Root Mean Squared Error
+        # print(len(grey_content) , len(generated))
+        # print(grey_content , generated)
+
+        if(len(generated)>len(grey_content)): 
+            end1 = len(generated)-len(grey_content)
+            for i in range(len(end1)):
+                grey_content = np.append(grey_content,0)
+        elif(len(generated)<len(grey_content)):
+            end = len(grey_content)-len(generated)
+            
+            for j in range(end):
+                
+                generated=np.append(generated,0)
+            
+        
+        loss = np.mean((grey_content-generated)**2)
+        
+
+        return loss
+    
+
+
+
+
+    #STYLE LOSS FUNCTIONS
+
+
+
+
+    #LAYER CREATION FUNCTIONS
+
+    def get_gaussian_filter(self,sigma): 
+
+        ksize = int(2*np.ceil(2*sigma)+1)
+        kernel = cv2.getGaussianKernel(ksize,sigma)
+
+        return np.outer(kernel,kernel)
+    
+    def get_gaussian_pyramid(self, image , levels): 
+
+        pyramid = [image.astype(np.float32)]
+
+        for i in range(1,levels): 
+            sigma = 2**i
+            blurred = cv2.filter2D(pyramid[-1],-1,self.get_gaussian_filter(sigma))
+            downsampled = blurred[::2,::2]
+            pyramid.append(downsampled)
+
+        return pyramid
+    
+    def define_layers(self, input_image , num_layers): 
+        # print(len(input_image) , "IMAGE SIZE ARRAY")
+        # print(type(input_image))
+     
+        # for i in range(len(input_image)): 
+        #     try:
+        #         print(len(input_image[i]))
+        #         new_arr.append(input_image[i])
+
+        #     except: 
+        #         print(input_image[i])
+
+        _numpy = np.asarray(input_image)
+
+        pyramid = self.get_gaussian_pyramid(_numpy,num_layers)
+
+        layers = []
+        
+        for i in range(num_layers):
+            layer_name = f'gaussian_{i}'
+            layer_output = pyramid[i]
+            layers.append((layer_name , layer_output))
+
+        return layers
+
+
+
+    #STYLE LOSS FUNCTIONS
+    #Stle
+    #SADSADASDADS
+    #Using Gram matrix to calculate style loss allows us to find the distance between the vectors 
+    def style_loss(self): 
+        input_grams  = [] 
+        style_grams = []
+
+        img2 = img.open(self.style_image)
+
+        # input_image = np.array(self.genes,dtype=np.float32)/255.0
+
+        # style_image = list(img2.getdata())
+        style_image = np.array(img2,dtype=np.float32)/255.0
+        
+        new_genes_arr = []
+      
+        for i in range(len(self.genes)): 
+            if type(self.genes[i]) != float:
+                # print(len(self.genes[i]))
+                new_genes_arr.append(self.genes[i])
+      
+                
+
+        layers = self.define_layers(new_genes_arr , 3)
+        
+        
+        for layer in layers: 
+            # input_feature_map = cv2.imread(r"C:\\Users\\RileyHall\\Documents\\Applied AI\\final\\output23jpg.jpg", cv2.IMREAD_GRAYSCALE) 0.917694778002361 0.917694778002361
+
+            input_feature_map = cv2.normalize(np.array(new_genes_arr),None,0,255,cv2.NORM_MINMAX,cv2.CV_8U)
+            style_feature_map = cv2.imread(self.style_image, cv2.IMREAD_GRAYSCALE)
+            # input_feature_map = np.dot(np.array(input_image)[...,:3],[0.2989, 0.5870, 0.1140])
+            # style_feature_map = np.dot(np.array(style_image)[...,:3],[0.2989, 0.5870, 0.1140])
+
+            input_feature_map = np.array(input_feature_map,dtype=np.float32)/255.0
+            style_feature_map = np.array(style_feature_map,dtype=np.float32)/255.0
+            # print(input_feature_map)
+            # print(input_feature_map.ndim)
+            if(input_feature_map.ndim==1):
+                length = input_feature_map.shape[0]
+
+                h,w =1,length
+            else:
+                h, w = input_feature_map.shape[:2]
+            style_feature_map = cv2.resize(style_feature_map,(h,w))
+
+            input_grams.append(self.gram_matrix(np.expand_dims(input_feature_map,1)))
+            style_grams.append(self.gram_matrix(np.expand_dims(style_feature_map,1)))
+        
+        loss = 0
+
+        for i in range(len(input_grams)):
+            _,c=input_grams[i].shape
+            size = c*c
+            if(input_grams[i].shape>style_grams[i].shape):
+                displace = input_grams[i].size-style_grams[i].size
+                # print("SIZE" , len(input_grams[i])-len(style_grams[i]))
+                # print(input_grams[i])
+                for j in range(displace):
+                    
+                    style_grams[i] = np.append(style_grams[i] , 0)
+                
+                style_grams[i]=style_grams[i].reshape(input_grams[i].shape)
+
+            loss+=np.sum((input_grams[i]-style_grams[i])**2)/size
+
+        return loss
+    
+
+    #The gram matrix is created by multiplying its self with the Transposed (replacing the rows with columns and columns with the rows) version of its self
+    #This will allow us to transfer the style across the style image 
+    def gram_matrix(self,feature_map):
+        h,w,c = feature_map.shape
+        flattend = np.reshape(feature_map,(h*w,c))
+        gram = np.dot(flattend.T,flattend)
+
+        return gram 
+    
--- src/style_transfer/genetic.py
+++ src/style_transfer/genetic.py
@@ -1,22 +1,29 @@
 import random
 import numpy as np
 from chromosome import Chromosome
-
+import matplotlib.pyplot as plt
+from PIL import Image as img
+MAX_GENERATION = 5000
+POPULATION_SIZE = 100
 def getPopulation(n):
     # get 100 chromosomes with n random genes
-    population = [Chromosome.randomize(n) for _ in range(100)]
+    population = [Chromosome(Chromosome.generate()) for _ in range(POPULATION_SIZE)]
     # print each chrom in population
-    print("Initial Population:")
-    for i in range(100):
-        print(" " + population[i].to_string())
+    # print("Initial Population:")
+    # for i in range(10): #POPULATION SIZE
+    #     print(" " + population[i].to_string())
+
 
     return population
 
 def getMutation(child):
     # get a random point to mutate a gene
-    mutation_point = random.randint(0, child.gene_count - 1)
+    mutation_pixel = random.randint(0, child.gene_count - 1)
+    mutation_point = random.randint(0, len(child.genes[mutation_pixel])-1)
+    
     # flip the gene bit
-    child.genes[mutation_point] ^= 1
+    # print( "CHILDREN GENES", child.genes , "CHILDREN GENES")
+    child.genes[mutation_pixel][mutation_point]= random.random()
 
     return child
 
@@ -29,8 +36,10 @@ def getChildren(parents):
     child_genes2 = parents[1].genes[:crossover_cut] + parents[0].genes[crossover_cut:]
 
     # create 2 child chroms with these gene sets
-    child1 = Chromosome(child_genes1, parents[0].gene_count)
-    child2 = Chromosome(child_genes2, parents[0].gene_count)
+    # child1 = Chromosome(child_genes1, parents[0].gene_count)
+    # child2 = Chromosome(child_genes2, parents[0].gene_count)
+    child1 = Chromosome(child_genes1)
+    child2 = Chromosome(child_genes2)
 
     # mutate both children
     child1 = getMutation(child1)
@@ -41,22 +50,23 @@ def getChildren(parents):
 
 def getParents(selection):
     # 2 highest fitnesses out of selection of 4
-    highest_fit1 = -1
-    highest_fit2 = -1
+    highest_fit1 = 100000000.00
+    highest_fit2 = 100000000.00
     # parents
     parent1 = None
     parent2 = None
 
+    
     # go through each chrom in selection
     for chromosome in selection:
         fitness = chromosome.get_fitness()
         # gets the top 2 highest fitness chroms in selection
-        if fitness > highest_fit1:
+        if fitness < highest_fit1:
             highest_fit2 = highest_fit1
             parent2 = parent1
             highest_fit1 = fitness
             parent1 = chromosome
-        elif fitness > highest_fit2:
+        elif fitness < highest_fit2:
             highest_fit2 = fitness
             parent2 = chromosome
 
@@ -80,13 +90,14 @@ def getSelection(population, gen , genes_num):
         selection = [population[idx] for idx in selection_indices]
 
         # print each chrom in selection
-        print("\nSelection #" + str(i + 1))
-        for chromosome in selection:
-            print(" " + chromosome.to_string())
+        # print("\nSelection #" + str(i + 1))
+        # for chromosome in selection:
+        #     print(" " + chromosome.to_string())
 
         # get the 2 parents for this selection
+        
         parents = getParents(selection)
-
+        
         # get the children of the parents
         children = getChildren(parents)
 
@@ -94,9 +105,9 @@ def getSelection(population, gen , genes_num):
         selection = parents + children
 
         # print each chrom in selection
-        print("\nSelection #" + str(i + 1) + " after breeding")
-        for chromosome in selection:
-            print(" " + chromosome.to_string())
+        # print("\nSelection #" + str(i + 1) + " after breeding")
+        # for chromosome in selection:
+        #     print(" " + chromosome.to_string())
 
         # add new selection to new population
         new_population += selection
@@ -106,25 +117,45 @@ def getSelection(population, gen , genes_num):
     random.shuffle(new_pop_indices)
     new_population = [new_population[idx] for idx in new_pop_indices]
 
-    for chrom in new_population:
-        fitness = chrom.get_fitness()
-        if fitness == genes_num:
-            print("\nThe Royal Road has been found in generation " + str(gen) + ". Congratulations " + chrom.to_string())
-            quit()
+    # for chrom in new_population:
+    #     fitness = chrom.get_fitness()
+    #     if fitness == genes_num:
+    #         # print("\nThe Royal Road has been found in generation " + str(gen) + ". Congratulations " + chrom.to_string())
+    #         quit()
 
     return new_population
 
 
 def main():
     # n = int(input("Please enter the number of genes you would like in each chromosome: "))
-    genes_num = 500
+    genes_num = 10
     first_gen = getPopulation(genes_num)
     n = 2
     while True:
         print("\nGeneration: " + str(n))
+        print(genes_num)
         next_population = getSelection(first_gen, n, genes_num)
         first_gen = next_population
         n += 1
+        print("GENERATION" , n)
+        if(n == MAX_GENERATION): 
+            fittest = 1000000000
+            index = 0
+            for i in range(len(first_gen)): 
+                if(fittest>first_gen[i].get_fitness()): 
+                    index = i
+                    fittest = first_gen[i].get_fitness()
+            # first_gen[index].to_string()
+            display = []
+            for ii in range(len(first_gen[index].genes)):
+                if(type(first_gen[index].genes[ii])!=float):
+                    display.append(first_gen[i].genes[ii])
+            plt.imshow(display)
+            plt.show()
+
+            break
+                
+
 
 
 if __name__ == '__main__':
